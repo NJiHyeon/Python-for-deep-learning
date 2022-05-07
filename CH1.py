16c82b4744209d522ff689c9428e3178d2faa3d7
JiHyeon Noh <ggpji59105@naver.com>
2022-05-08 01:05:49 +0900

    220505_01:05

--- CH1.py
+++ CH1.py
@@ -1,14 +1,8 @@
-#타입
-#연산
-#라이브러리
-from pickle import FALSE, TRUE
-
-
+#1.연산
 a = 1
 b = 2
 c = a + b
 print(c)
-print("hello, world!")
 
 a = 1.2
 b = 2.4
@@ -17,8 +11,47 @@ print(a+b)
 a = 's'
 b = "q"
 print(a+b)
+print(a*b)
 
+#2.여러가지 타입의 형태
 a = True
 b = False
 print(a+b)
 print(type(a))
+
+aa = 4
+print(type(a))
+
+bb = 4.3
+print(type(a))
+
+cc = 'hi'
+print(type(a))
+
+list1 = [1,2,3,4]
+arr = numpy.array([1,2,3,4])
+tuple1 = (1,2)
+print(type(list1))
+print(type(arr))
+print(type(tuple1))
+
+#3.라이브러리
+import numpy
+#zeros, empty, ones, array, sum, .....
+arr1 = numpy.array([1,2,3,4])
+arr2 = numpy.zeros(4) #1행 4열짜리 영행렬
+
+import numpy as np
+arr1 = np.array([1,2,3,4])
+arr2 = np.zeros(4)
+arr3 = np.ones(4)
+
+from numpy import array, ones, zeros
+arr1 = array([1,2,3,4])
+arr2 = zeros(4)
+arr3 = ones(4)
+
+from random import sample
+import numpy as np
+import pandas as pd
+import tensorflow as tf
