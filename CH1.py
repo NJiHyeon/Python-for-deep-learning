815017843adf3c11cb3aed6b1b4fcd00e91f5999
JiHyeon Noh <ggpji59105@naver.com>
2022-05-07 20:29:50 +0900

    220507_20:30

--- CH1.py
+++ CH1.py
@@ -1,4 +1,24 @@
 #타입
 #연산
 #라이브러리
-print("hello")
+from pickle import FALSE, TRUE
+
+
+a = 1
+b = 2
+c = a + b
+print(c)
+print("hello, world!")
+
+a = 1.2
+b = 2.4
+print(a+b)
+
+a = 's'
+b = "q"
+print(a+b)
+
+a = True
+b = False
+print(a+b)
+print(type(a))
