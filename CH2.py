84ce6e7e7c90d57ff75ee018f94a64b49581cce9
JiHyeon Noh <ggpji59105@naver.com>
2022-05-08 01:15:28 +0900

    1

---
+++ CH2.py
@@ -0,0 +1,11 @@
+# numpy
+# if, for, while
+import numpy as np
+arr1 = np.array([1,2,3,4])
+arr2 = np.zeros((2,3))
+arr3 = np.arange(10) #0부터 10개까지의 배열
+arr4 = np.random.randint(1,45,6) #1부터 45사이에서 6개 뽑기
+print(arr1)
+print(arr2)
+print(arr3)
+print(arr4)
